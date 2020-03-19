aea220c3958bf779b9a8e947b1b098695d915c24
LouisSalomon <47212378+LouisSalomon@users.noreply.github.com>
2020-03-19 15:12:37 +0100

    Ajout du projet de base

---
+++ Explorer/Explorer/Explorer/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.btnSortie = new System.Windows.Forms.Button();
+            this.lblSortie = new System.Windows.Forms.Label();
+            this.txtSortie = new System.Windows.Forms.TextBox();
+            this.btnGenerer = new System.Windows.Forms.Button();
+            this.btnExplorer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSource.Location = new System.Drawing.Point(230, 23);
+            this.txtSource.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(1112, 31);
+            this.txtSource.TabIndex = 0;
+            this.txtSource.Text = "C:\\temp\\IUT\\Explorer";
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(24, 29);
+            this.lblSource.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(195, 25);
+            this.lblSource.TabIndex = 1;
+            this.lblSource.Text = "Répertoire source :";
+            // 
+            // btnSource
+            // 
+            this.btnSource.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSource.Location = new System.Drawing.Point(1358, 19);
+            this.btnSource.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(220, 44);
+            this.btnSource.TabIndex = 2;
+            this.btnSource.Text = "Parcourir";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // btnSortie
+            // 
+            this.btnSortie.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSortie.Location = new System.Drawing.Point(1358, 69);
+            this.btnSortie.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSortie.Name = "btnSortie";
+            this.btnSortie.Size = new System.Drawing.Size(220, 44);
+            this.btnSortie.TabIndex = 5;
+            this.btnSortie.Text = "Parcourir";
+            this.btnSortie.UseVisualStyleBackColor = true;
+            this.btnSortie.Click += new System.EventHandler(this.btnSortie_Click);
+            // 
+            // lblSortie
+            // 
+            this.lblSortie.AutoSize = true;
+            this.lblSortie.Location = new System.Drawing.Point(24, 79);
+            this.lblSortie.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblSortie.Name = "lblSortie";
+            this.lblSortie.Size = new System.Drawing.Size(178, 25);
+            this.lblSortie.TabIndex = 4;
+            this.lblSortie.Text = "Fichier de sortie :";
+            // 
+            // txtSortie
+            // 
+            this.txtSortie.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSortie.Location = new System.Drawing.Point(230, 73);
+            this.txtSortie.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtSortie.Name = "txtSortie";
+            this.txtSortie.Size = new System.Drawing.Size(1112, 31);
+            this.txtSortie.TabIndex = 3;
+            this.txtSortie.Text = "C:\\temp\\test.txt";
+            // 
+            // btnGenerer
+            // 
+            this.btnGenerer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerer.Location = new System.Drawing.Point(30, 125);
+            this.btnGenerer.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnGenerer.Name = "btnGenerer";
+            this.btnGenerer.Size = new System.Drawing.Size(1548, 81);
+            this.btnGenerer.TabIndex = 6;
+            this.btnGenerer.Text = "Lister les fichiers";
+            this.btnGenerer.UseVisualStyleBackColor = true;
+            this.btnGenerer.Click += new System.EventHandler(this.btnGenerer_Click);
+            // 
+            // btnExplorer
+            // 
+            this.btnExplorer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExplorer.Location = new System.Drawing.Point(30, 377);
+            this.btnExplorer.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnExplorer.Name = "btnExplorer";
+            this.btnExplorer.Size = new System.Drawing.Size(1548, 75);
+            this.btnExplorer.TabIndex = 8;
+            this.btnExplorer.Text = "Ouvrir l\'explorer complet";
+            this.btnExplorer.UseVisualStyleBackColor = true;
+            this.btnExplorer.Click += new System.EventHandler(this.btnExplorer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1602, 475);
+            this.Controls.Add(this.btnExplorer);
+            this.Controls.Add(this.btnGenerer);
+            this.Controls.Add(this.btnSortie);
+            this.Controls.Add(this.lblSortie);
+            this.Controls.Add(this.txtSortie);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.txtSource);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Button btnSortie;
+        private System.Windows.Forms.Label lblSortie;
+        private System.Windows.Forms.TextBox txtSortie;
+        private System.Windows.Forms.Button btnGenerer;
+        private System.Windows.Forms.Button btnExplorer;
+    }
+}
+
---
+++ Explorer/Explorer/Explorer/frmExplorer.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Explorer
+{
+    partial class frmExplorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tvRepertoires = new System.Windows.Forms.TreeView();
+            this.lvFiles = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSource.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtSource.Location = new System.Drawing.Point(0, 0);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ReadOnly = true;
+            this.txtSource.Size = new System.Drawing.Size(804, 20);
+            this.txtSource.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 20);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tvRepertoires);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lvFiles);
+            this.splitContainer1.Size = new System.Drawing.Size(804, 575);
+            this.splitContainer1.SplitterDistance = 268;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // tvRepertoires
+            // 
+            this.tvRepertoires.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvRepertoires.Location = new System.Drawing.Point(0, 0);
+            this.tvRepertoires.Name = "tvRepertoires";
+            this.tvRepertoires.Size = new System.Drawing.Size(268, 575);
+            this.tvRepertoires.TabIndex = 0;
+            this.tvRepertoires.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.tvRepertoires_BeforeExpand);
+            this.tvRepertoires.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvRepertoires_AfterSelect);
+            // 
+            // lvFiles
+            // 
+            this.lvFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvFiles.Location = new System.Drawing.Point(0, 0);
+            this.lvFiles.Name = "lvFiles";
+            this.lvFiles.Size = new System.Drawing.Size(532, 575);
+            this.lvFiles.TabIndex = 0;
+            this.lvFiles.UseCompatibleStateImageBehavior = false;
+            // 
+            // frmExplorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 595);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.txtSource);
+            this.Name = "frmExplorer";
+            this.Text = "frmExplorer";
+            this.Load += new System.EventHandler(this.frmExplorer_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView tvRepertoires;
+        private System.Windows.Forms.ListView lvFiles;
+    }
+}
